e7602cbac0ddd54efe6071fa3ae5902f23a76e98
Sasy07 <sasan.ghafooryfard@gmail.com>
2024-07-19 02:17:08 +0330

    Init CQRS

---
+++ Backend/Store/Src/Application/Features/Products/Queries/GetAll/GetAllProductsQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Features.Product.Queries.GetAll;
+
+public class GetAllProductsQuery
+{
+    
+}
---
+++ Backend/Store/Src/Application/Features/Products/Queries/GetAll/GetAllProductsQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Features.Product.Queries.GetAll;
+
+public class GetAllProductsQueryHandler:IRequest<IEnumerable<Domain.Entities.Product>>
+{
+    
+}
