594ca1fb61fd9f4e86b51d3515cfa1aa6a899478
Sasy07 <sasan.ghafooryfard@gmail.com>
2024-08-06 19:30:18 +0330

    EduStoreClient (Angular) init

---
+++ Backend/Store/Src/Application/Wrappers/PaginationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Wrappers;
+
+public class PaginationResponse<T>where T : class
+{
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int Count { get; set; }
+    public IReadOnlyList<T> Result { get; set; }
+}
